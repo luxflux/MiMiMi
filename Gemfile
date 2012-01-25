9ffcbb41fa700119abf612d5d88c2b271f1f3dca
Raffael Schmid <raffael@yux.ch>
2012-01-26 00:13:55 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.0'
+gem 'sqlite3'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+  gem 'compass', '0.12.alpha.4'
+  gem 'compass-h5bp'
+end
+
+gem 'haml-rails'
+
+gem 'jquery-rails'
+gem 'html5-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiMiMi::Application.config.secret_token = '1b3dd33262f5d8de3c2fa553674e2ae92a2b9a1a5a019191f2ad8a230aec4684a23dfe550d554dd9087aba6f25e85d4d2679dc56744d8f015b657fd40212933d'
