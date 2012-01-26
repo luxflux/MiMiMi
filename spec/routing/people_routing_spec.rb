7a4a0b6b5c343b543e63d11b9792f4fd77d2a876
Raffael Schmid <raffael@yux.ch>
2012-01-26 22:13:31 +0100

    there is no Person#show

--- spec/routing/people_routing_spec.rb
+++ spec/routing/people_routing_spec.rb
@@ -11,9 +11,9 @@ describe PeopleController do
       get("/people/new").should route_to("people#new")
     end
 
-    it "routes to #show" do
-      get("/people/1").should route_to("people#show", :id => "1")
-    end
+#    it "routes to #show" do
+#      get("/people/1").should route_to("people#show", :id => "1")
+#    end
 
 #    it "routes to #edit" do
 #      get("/people/1/edit").should route_to("people#edit", :id => "1")
