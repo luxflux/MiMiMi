5b32ab306293ea40048605585d7cd1c188757acb
Raffael Schmid <raffael@yux.ch>
2012-01-26 20:52:19 +0100

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120126192027) do
+ActiveRecord::Schema.define(:version => 20120126193358) do
 
   create_table "friendly_id_slugs", :force => true do |t|
     t.string   "slug",                         :null => false
