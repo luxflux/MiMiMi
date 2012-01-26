2a24b29dad3e8d666dbfb8cf5003bf2b1727c20f
Raffael Schmid <raffael@yux.ch>
2012-01-26 20:48:38 +0100

    nest Quote inside Person

--- spec/routing/quotes_routing_spec.rb
+++ spec/routing/quotes_routing_spec.rb
@@ -4,28 +4,28 @@ describe QuotesController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/quotes").should route_to("quotes#index")
+      get("/people/1/quotes").should route_to("quotes#index", "person_id" => "1")
     end
 
     it "routes to #new" do
-      get("/quotes/new").should route_to("quotes#new")
+      get("/people/1/quotes/new").should route_to("quotes#new", "person_id" => "1")
     end
 
     it "routes to #show" do
       get("/quotes/1").should route_to("quotes#show", :id => "1")
     end
 
-    it "routes to #edit" do
-      get("/quotes/1/edit").should route_to("quotes#edit", :id => "1")
-    end
+#    it "routes to #edit" do
+#      get("/quotes/1/edit").should_not route_to("quotes#edit", :id => "1")
+#    end
 
     it "routes to #create" do
-      post("/quotes").should route_to("quotes#create")
+      post("/people/1/quotes").should route_to("quotes#create", "person_id" => "1")
     end
 
-    it "routes to #update" do
-      put("/quotes/1").should route_to("quotes#update", :id => "1")
-    end
+#    it "routes to #update" do
+#      put("/quotes/1").should route_to("quotes#update", :id => "1")
+#    end
 
     it "routes to #destroy" do
       delete("/quotes/1").should route_to("quotes#destroy", :id => "1")
